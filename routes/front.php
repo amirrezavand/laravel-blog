c6df260024af1b7312609d7c7b4395431fe89145
rezavand <ar.rezavand@mrc.ir>
2022-04-13 17:37:10 +0430

    edit top menu bar items

--- routes/front.php
+++ routes/front.php
@@ -7,8 +7,9 @@ use App\Http\Controllers\Front\AboutUsController;
 use App\Http\Controllers\Front\HomeController;
 
 Route::get('home',[HomeController::class,'index'])->name('home');
-Route::get('blogs',[BlogController::class,'index']);
+Route::get('blogs',[BlogController::class,'index'])->name('blog');
 Route::get('blogs/{blog}',[BlogController::class,'single']);
-Route::get('contact_us',[ContactUsController::class,'index']);
-Route::get('about_us',[AboutUsController::class,'aboutUs']);
-Route::get('our_team',[AboutUsController::class,'ourTeam']);
+Route::get('contact_us',[ContactUsController::class,'index'])->name('contact_us');
+Route::get('about_us',[AboutUsController::class,'aboutUs'])->name('about_us');
+Route::get('our_team',[AboutUsController::class,'ourTeam'])->name('our_team');
+
