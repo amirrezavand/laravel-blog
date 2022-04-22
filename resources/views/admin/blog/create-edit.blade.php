474c4772b5506d92da0c243debf915c8a5a57a98
amirrezavand <ar.rezavand@gmail.com>
2022-04-22 06:11:28 -0700

    edit breadcrumb blog

--- resources/views/admin/blog/create-edit.blade.php
+++ resources/views/admin/blog/create-edit.blade.php
@@ -11,8 +11,9 @@
             <div class="col-12 col-md-6 order-md-2 order-first">
                 <nav aria-label="breadcrumb" class="breadcrumb-header float-start float-lg-end">
                     <ol class="breadcrumb">
-                        <li class="breadcrumb-item"><a href="index.html">Dashboard</a></li>
-                        <li class="breadcrumb-item active" aria-current="page">Table</li>
+                        <li class="breadcrumb-item"><a href="/admin/">Dashboard</a></li>
+                        <li class="breadcrumb-item"><a href="{{route('admin.blog.index')}}">Blog</a></li>
+                        <li class="breadcrumb-item active" aria-current="page">Create and Edit</li>
                     </ol>
                 </nav>
             </div>
