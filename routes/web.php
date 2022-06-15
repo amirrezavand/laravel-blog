c5a718456f263cf384528c450867b9290869be1e
amirrezavand <ar.rezavand@gmail.com>
2022-06-15 12:45:02 -0700

    route main page was changed

--- routes/web.php
+++ routes/web.php
@@ -13,15 +13,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/dashboard', function () {
-    alert()->success('this is text','this is title');
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
-
-require __DIR__.'/auth.php';
-Route::get('/auth/google',[\App\Http\Controllers\Auth\GoogleAuthController::class,'redirect'])->name('auth.google');
-Route::get('/auth/google/callback',[\App\Http\Controllers\Auth\GoogleAuthController::class,'callback']);
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+//
+//Route::get('/dashboard', function () {
+//    alert()->success('this is text','this is title');
+//    return view('dashboard');
+//})->middleware(['auth'])->name('dashboard');
+//
+//require __DIR__.'/auth.php';
+//Route::get('/auth/google',[\App\Http\Controllers\Auth\GoogleAuthController::class,'redirect'])->name('auth.google');
+//Route::get('/auth/google/callback',[\App\Http\Controllers\Auth\GoogleAuthController::class,'callback']);
--- routes/web/front.php
+++ routes/web/front.php
@@ -6,12 +6,12 @@ use App\Http\Controllers\Front\ContactUsController;
 use App\Http\Controllers\Front\AboutUsController;
 use App\Http\Controllers\Front\HomeController;
 
-Route::get('home',[HomeController::class,'index'])->name('home');
-Route::get('blogs',[BlogController::class,'index'])->name('blog');
-Route::get('blogs/{blog}',[BlogController::class,'single']);
-Route::get('contact_us',[ContactUsController::class,'index'])->name('contact_us');
-Route::get('about_us',[AboutUsController::class,'aboutUs'])->name('about_us');
-Route::get('our_team',[AboutUsController::class,'ourTeam'])->name('our_team');
+Route::get('/',[HomeController::class,'index'])->name('home');
+Route::get('/blogs',[BlogController::class,'index'])->name('blog');
+Route::get('/blogs/{blog}',[BlogController::class,'single']);
+Route::get('/contact_us',[ContactUsController::class,'index'])->name('contact_us');
+Route::get('/about_us',[AboutUsController::class,'aboutUs'])->name('about_us');
+Route::get('/our_team',[AboutUsController::class,'ourTeam'])->name('our_team');
 
 Route::get('login',function (){
     return view('front.auth.login');
