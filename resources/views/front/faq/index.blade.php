316c9883252f506a2cb371d73eccb8368fbd60c1
Administrator <admin@example.com>
2022-12-13 10:22:53 -0800

    add faq page

--- resources/views/front/faq/index.blade.php
+++ resources/views/front/faq/index.blade.php
@@ -97,21 +97,80 @@
                         </div>
 
                         <!-- general Query -->
-                        <div class="tab-pane fade" id="item2" role="tabpanel" aria-labelledby="payment-tab">
+                        <div class="tab-pane fade" id="item2" role="tabpanel" aria-labelledby="upgrade-tab">
 
-                            <div class="accordion" id="paymentac">
+                            <div class="accordion" id="parent2">
                                 <div class="card">
-                                    <div class="card-header" id="headingOne1">
+                                    <div class="card-header" id="headingOne2">
                                         <h2 class="mb-0">
-                                            <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#paymentcollapse" aria-expanded="true" aria-controls="paymentcollapse">
-                                                آیا می توانم درخواست بازپرداخت کنم؟
+                                            <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#upgradecollapseOne" aria-expanded="true" aria-controls="upgradecollapseOne">
+                                                نحوه برگزاری دوره ها چگونه است؟
                                             </button>
                                         </h2>
                                     </div>
 
-                                    <div id="paymentcollapse" class="collapse show" aria-labelledby="headingOne1" data-parent="#paymentac">
+                                    <div id="upgradecollapseOne" class="collapse show" aria-labelledby="headingOne2" data-parent="#parent2">
                                         <div class="card-body">
-                                            <p class="ac-para">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد. کتابهای زیادی در شصت و سه درصد گذشته، حال و آینده شناخت فراوان جامعه و متخصصان را می طلبد تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان خلاقی و فرهنگ پیشرو در زبان فارسی ایجاد کرد. در این صورت می توان امید داشت که تمام و دشواری موجود در ارائه راهکارها و شرایط سخت تایپ به پایان رسد وزمان مورد نیاز شامل حروفچینی دستاوردهای اصلی و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.</p>
+                                            <p class="ac-para">دوره ها به سه طریق در کافه جی آی اس برگزار می شود.</p>
+                                            <ul style="list-style-type: disc;padding-right: 20px">
+                                                <li>دوره های غیر حضوری: دوره های غیر حضوری دوره هایی است که مطالب آموزشی به صورت بسته های آموزشی ارائه شده و در قالب فیلم های آموزشی یه صورت دانلودی در اختیار شما دانشجویان عزیز قرار می گیرد، دوره های غیرحضوری شامل پشتیبانی مدرسین CaffeGIS می باشند.</li>
+                                                <li>دوره های آنلاین: دوره های آنلاین شبیه به دوره های حضوری بوده با این تفاوت که به صورت آنلاین در محیط Adobe Connect برگزار می گردد. جلسات کلاس ضبط گردیده و در پایان دوره در پروفایل دانشچو در دسترس قرار می گیرد. دوره های آنلاین نیز مشابه دوره های دیگر شامل پشتیبانی مدرسین CaffeGIS می باشد.</li>
+                                                <li>دوره های حضوری: در دوره های حضوری دانشجویان در کلاس حضور فیزیکی داشته و آموزش ها به صورت چهره به چهره می باشد. این دوره ها به صورت کلاس های حضوری چند نفره در آموزشگاه ها و موسسات آموزش عالی و نیز سمینارهای آموزشی برای دانشگاه ها، شرکت ها و سازمان های مختلف دولتی و حصوصی برگزار می گردد. این دوره ها همراه با پشتیبانی آنلاین مدرسین بوده و در انتهای هر دوره فیلم آموزشی در اختیار دانشجوی گرامی قرار می گیرد.</li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="card">
+                                    <div class="card-header" id="headingTwo2">
+                                        <h2 class="mb-0">
+                                            <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseTwo2">
+                                                سر فصل های دوره های حضوری، آنلاین و غیر حضوری چه تفاوتی دارد؟
+                                            </button>
+                                        </h2>
+                                    </div>
+
+                                    <div id="collapseTwo2" class="collapse" aria-labelledby="headingTwo2" data-parent="#parent2">
+                                        <div class="card-body">
+                                            <p class="ac-para">
+                                                سر فصل ها هیچ تفاوتی ندارند و همه دوره ها مبتنی بر مسیر یادگیری است. صرفا نحوه برگزاری دوره متفاوت است.
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="card">
+                                    <div class="card-header" id="headingThree2">
+                                        <h2 class="mb-0">
+                                            <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseThree2">
+                                                نحوه ثبت نام در دوره های حضوری چگونه است؟
+                                            </button>
+                                        </h2>
+                                    </div>
+
+                                    <div id="collapseThree2" class="collapse" aria-labelledby="headingThree2" data-parent="#parent2">
+                                        <div class="card-body">
+                                            <p class="ac-para">
+                                                برنامه دوره های حضوری و آنلاین در تقویم آموزشی آورده شده است. کافیست روی دوره مورد نظر کلیک کرده و دکمه رزرو را کلیک کنید. می توانی زمان دوره را مشاهده کرده و با پرداخت یک چهارم مبلغ، در دوره پیش ثبت نام کنید.
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="card">
+                                    <div class="card-header" id="headingFour2">
+                                        <h2 class="mb-0">
+                                            <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseFour2">
+                                                نحوه شرکت در دوره های آنلاین چگونه است؟
+                                            </button>
+                                        </h2>
+                                    </div>
+
+                                    <div id="collapseFour2" class="collapse" aria-labelledby="headingFour2" data-parent="#parent2">
+                                        <div class="card-body">
+                                            <p class="ac-para">
+                                                مشابه دوره های حضوری باید پیش ثبت نام انجام دهید.
+                                            </p>
                                         </div>
                                     </div>
                                 </div>
@@ -122,23 +181,78 @@
                         <!-- general Query -->
                         <div class="tab-pane fade" id="item3" role="tabpanel" aria-labelledby="upgrade-tab">
 
-                            <div class="accordion" id="upgradeac">
+                            <div class="accordion" id="parent3">
                                 <div class="card">
-                                    <div class="card-header" id="headingOne2">
+                                    <div class="card-header" id="headingOne3">
                                         <h2 class="mb-0">
-                                            <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#upgradecollapseOne" aria-expanded="true" aria-controls="upgradecollapseOne">
-                                                چگونه تم Learnup را ارتقا دهیم؟
+                                            <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne3">
+                                                مسیر یادگیری چیست و بر چه اساسی طراحی شده است؟
                                             </button>
                                         </h2>
                                     </div>
 
-                                    <div id="upgradecollapseOne" class="collapse show" aria-labelledby="headingOne2" data-parent="#upgradeac">
+                                    <div id="collapseOne3" class="collapse show" aria-labelledby="headingOne3" data-parent="#parent2">
                                         <div class="card-body">
-                                            <p class="ac-para">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد. کتابهای زیادی در شصت و سه درصد گذشته، حال و آینده شناخت فراوان جامعه و متخصصان را می طلبد تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان خلاقی و فرهنگ پیشرو در زبان فارسی ایجاد کرد. در این صورت می توان امید داشت که تمام و دشواری موجود در ارائه راهکارها و شرایط سخت تایپ به پایان رسد وزمان مورد نیاز شامل حروفچینی دستاوردهای اصلی و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.</p>
+                                            <p class="ac-para">
+                                                مسیر یادگیری در حقیقت نقشه راه شما برای متخصص شدن شماست. این مسیرها شامل تعداد زیادی جلسه آموزشی، تعداد زیادی ایستگاه تمرین و پرسش و ایستگاه تمرین و پرسش و ایستگاه های چالش است که با عبور موفقیت آمیز از هر مرحله به مرحله بعد خواهید رفت. ایستگاه های چالش همراه با اعتبار هدیه و ایستگاه های تمرین نمره شما را در پایان دوره مشخص می کند. همچنین روند پیشرفت دوره شما وابسته به مشاهده ویدئوها یا شرکت در کلاس ها مشخص می شود. مسیرهای یادگیری مبتنی بر مسیرهای یادگیری جهانی و منابع معتبر جهانی طراحی شده است. این مسیرها با مسیرهای شغلی نیز ترکیب شده و خروجی آن متخصص کردن شما برای حضور در فضای کسب و کار است.
+                                            </p>
                                         </div>
                                     </div>
                                 </div>
 
+                                <div class="card">
+                                    <div class="card-header" id="headingTwo3">
+                                        <h2 class="mb-0">
+                                            <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseTwo3">
+                                                چگونه باید مسیر یادگیری را طی نمایید؟
+                                            </button>
+                                        </h2>
+                                    </div>
+
+                                    <div id="collapseTwo3" class="collapse" aria-labelledby="headingTwo3" data-parent="#parent3">
+                                        <div class="card-body">
+                                            <p class="ac-para">
+                                                فارغ از اینکه نحوه حضور شما در دوره چگونه باشد می توانید در مسیر قرار بگیرید. عبور از مسیر زمان دار است و شما باید طبق واحد زمانی در نظر گرفته شده مسیر را طی نمایید. اگر در طی زمان مسیر را طی نمایید مزایایی چون پشتیبانی، منتورینگ، اعتبار هدیه خواهید داشت و در صورتی که نتوانید مسیر را در زمان مقرر به پایان برسانید می توانید با پرداخت 20 درصد مبلغ ثبت نام مجددا مسیر را تمدید نمایید.
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="card">
+                                    <div class="card-header" id="headingThree3">
+                                        <h2 class="mb-0">
+                                            <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseThree3">
+                                                با اعتبار هدیه چه کار می توانم انجام دهم؟
+                                            </button>
+                                        </h2>
+                                    </div>
+
+                                    <div id="collapseThree3" class="collapse" aria-labelledby="headingThree3" data-parent="#parent3">
+                                        <div class="card-body">
+                                            <p class="ac-para">
+                                               می توانید با جمع شدن سکه هایتان اعتبار هدیه دریافت کرده و هر محصولی از سایت را دوست داشتید بخرید.
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="card">
+                                    <div class="card-header" id="headingFour3">
+                                        <h2 class="mb-0">
+                                            <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseFour3">
+                                                شرکت در دوره ها تخفیف ندارد؟
+                                            </button>
+                                        </h2>
+                                    </div>
+
+                                    <div id="collapseFour3" class="collapse" aria-labelledby="headingFour3" data-parent="#parent3">
+                                        <div class="card-body">
+                                            <p class="ac-para">
+                                                دو نوع تخفیف داریم یک نوع تخفیف متناسب با هر دوره که برای هر دوره متناسب با زمان و قیمت دوره تخفیفاتی در نظر گرفته شده است که می توانید در صفحه مربوط به همان دوره مشاهده کنید. تخفیفات دیگر مربوط به ثبت نام پله ای در دوره های مسیر است که به ازای شرکت در هر دوره 5 درصد تخفیف میگیرید.
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
 
                         </div>
@@ -146,22 +260,43 @@
                         <!-- general Query -->
                         <div class="tab-pane fade" id="item4" role="tabpanel" aria-labelledby="upgrade-tab">
 
-                            <div class="accordion" id="upgradeac">
+                            <div class="accordion" id="parent4">
                                 <div class="card">
-                                    <div class="card-header" id="headingOne2">
+                                    <div class="card-header" id="headingOne4">
                                         <h2 class="mb-0">
-                                            <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#upgradecollapseOne" aria-expanded="true" aria-controls="upgradecollapseOne">
-                                                نحوه برگزاری دوره ها چگونه است؟
+                                            <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne4">
+                                                برای شرکت در دوره آنلاین چه تجهیزاتی مورد نیاز است؟
                                             </button>
                                         </h2>
                                     </div>
 
-                                    <div id="upgradecollapseOne" class="collapse show" aria-labelledby="headingOne2" data-parent="#upgradeac">
+                                    <div id="collapseOne4" class="collapse show" aria-labelledby="headingOne4" data-parent="#parent4">
                                         <div class="card-body">
-                                            <p class="ac-para">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد. کتابهای زیادی در شصت و سه درصد گذشته، حال و آینده شناخت فراوان جامعه و متخصصان را می طلبد تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان خلاقی و فرهنگ پیشرو در زبان فارسی ایجاد کرد. در این صورت می توان امید داشت که تمام و دشواری موجود در ارائه راهکارها و شرایط سخت تایپ به پایان رسد وزمان مورد نیاز شامل حروفچینی دستاوردهای اصلی و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.</p>
+                                            <p class="ac-para">
+                                                رایانه یا گوشی و اینترنت مناسب برای شرکت در کلاس کافیست.
+                                            </p>
                                         </div>
                                     </div>
                                 </div>
+                                <div class="card">
+                                    <div class="card-header" id="headingTwo4">
+                                        <h2 class="mb-0">
+                                            <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseTwo4">
+                                                شرکت در دوره های آنلاین با چه نرم افزارهایی امکان پذیر است؟
+                                            </button>
+                                        </h2>
+                                    </div>
+
+                                    <div id="collapseTwo4" class="collapse" aria-labelledby="headingTwo4" data-parent="#parent4">
+                                        <div class="card-body">
+                                            <p class="ac-para">
+                                                پلتفرم انتخاب ما ادوبی کانکت است.
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+
+
                             </div>
 
                         </div>
