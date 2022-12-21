b91efc8dc5d91a021f0aa32ca62fbedc8fd50c85
Administrator <admin@example.com>
2022-12-21 01:50:32 -0800

    edit some page

--- app/Http/Middleware/IsAdmin.php
+++ app/Http/Middleware/IsAdmin.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class IsAdmin
 {
@@ -16,8 +17,6 @@ class IsAdmin
      */
     public function handle(Request $request, Closure $next)
     {
-
-
         if (Auth::user() &&  Auth::user()->admin == 1) {
             return $next($request);
         }
