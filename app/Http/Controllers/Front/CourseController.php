c588763cc4c7b3b249cb08b48355d20a140a9198
Administrator <admin@example.com>
2023-02-05 00:04:40 -0800

    add tag for course

--- app/Http/Controllers/Front/CourseController.php
+++ app/Http/Controllers/Front/CourseController.php
@@ -5,16 +5,28 @@ namespace App\Http\Controllers\Front;
 use App\Http\Controllers\Controller;
 use App\Models\Course;
 use App\Models\CourseContent;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Artesaos\SEOTools\Traits\SEOTools as SEOToolsTrait;
 
 class CourseController extends Controller
 {
     use SEOToolsTrait;
-    public function index(){
+    public function index(Request $request){
         $this->seo()->setTitle('دوره ها');
         $this->seo()->setDescription('آموزش نرم افزار GIS و Google Earth ( گوگل ارث) و سایر نرم افزارهای نقشه برداری را توسط برترین مدرسین ایران ...');
-        $courses=Course::leftJoin('users','users.id','=','courses.user_id')->select(DB::raw('courses.*'),DB::raw('users.name as teacher_name'),DB::raw('users.avatar as teacher_avatar'))->orderby('id','desc')->paginate(6);
+
+        $tag=$request->input('tag');
+
+        if(is_null($tag)){
+            $courses=Course::leftJoin('users','users.id','=','courses.user_id')->select(DB::raw('courses.*'),DB::raw('users.name as teacher_name'),DB::raw('users.avatar as teacher_avatar'))->orderby('id','desc')->paginate(6);
+        }else {
+
+            $courses=Course::leftJoin('users','users.id','=','courses.user_id')->whereHas('tags', function($q) use ($tag) {
+                $q->where('title', '=', $tag);
+            })->select(DB::raw('courses.*'),DB::raw('users.name as teacher_name'),DB::raw('users.avatar as teacher_avatar'))->orderby('id','desc')->paginate(6);
+        }
+
         saveSeen('course',0);
         return view('front.course.index',compact('courses'));
     }
