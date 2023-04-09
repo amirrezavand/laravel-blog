fcc8f740c89ff257f3363d9a4e9fa227dd234738
amirrezavand <ar.rezavand@gmail.com>
2023-04-09 03:40:48 -0700

    some edit in category

--- app/Http/Controllers/Front/HomeController.php
+++ app/Http/Controllers/Front/HomeController.php
@@ -18,7 +18,7 @@ class HomeController extends Controller
         $this->seo()->setTitle('صفحه اصلی');
         $this->seo()->setDescription('حرفه ای ترین آموزش های نرم افزار GIS ، Google Earth، نقشه برداری با کافه جی آی اس.');
 
-        $courses=Course::leftJoin('users','users.id','=','courses.user_id')->select(DB::raw('courses.*'),DB::raw('users.name as teacher_name'),DB::raw('users.avatar as teacher_avatar'))->orderby('id','asc')->limit(6)->get();
+        $courses=Course::leftJoin('users','users.id','=','courses.user_id')->where('course.lu_content_status','pulish')->select(DB::raw('courses.*'),DB::raw('users.name as teacher_name'),DB::raw('users.avatar as teacher_avatar'))->orderby('id','asc')->limit(6)->get();
         $blogs=Blog::leftJoin('users','users.id','=','blogs.user_id')->select(DB::raw('blogs.*'),DB::raw('users.name as author_name'),DB::raw('users.avatar as author_avatar'))->orderby('id','desc')->limit(3)->get();
         return view('front.home.index',compact('blogs','courses'));
     }
